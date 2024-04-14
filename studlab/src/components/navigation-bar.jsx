62ea163407d761e07f68a5549c1d6f5b335d2fd2
Marcos150 <marcosruizrubio@gmail.com>
2024-04-14 13:52:48 +0200

    Añadido componente de barra de navegación

---
+++ studlab/src/components/navigation-bar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function NavigationBar() {
+  return (
+    <div className="nav-bar">
+        <img src="/home.png" alt="Home"></img>
+        <img src="/search.png" alt="Search"></img>
+        <img src="/plus.png" alt="New"></img>
+        <img src="/notification.png" alt="Notifications"></img>
+        <img src="/user.png" alt="Profile"></img>
+    </div>
+  )
+}
+
+export default NavigationBar;
--- studlab/src/pages/document-page.jsx
+++ studlab/src/pages/document-page.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import DocumentVisualizer from "../components/document-visualizer";
+import NavigationBar from "../components/navigation-bar";
+
 function DocumentPage({data}){
     return(
-        <DocumentVisualizer data={data}></DocumentVisualizer>
+        <div>
+            <DocumentVisualizer data={data}></DocumentVisualizer>
+            <NavigationBar></NavigationBar>
+        </div>
     )
 }
 export default DocumentPage;
