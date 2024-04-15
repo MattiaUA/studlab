9a20f63362cbcf0128462bf3d83eeb3ef5d34ccc
Mattia Sorella <sorellamattia@gmail.com>
2024-04-15 18:01:25 +0200

    Inicio

---
+++ studlab/src/components/layouts/login.jsx
@@ -0,0 +1,10 @@
+import '../../index.css'
+import style from './layouts.module.css'
+
+export default function Login() {
+    return (
+        <div className={style['login-container']}>
+            <img src='src\assets\images\login-bg.png' alt='imagen-fondo-login'></img>
+        </div>
+    )
+}
