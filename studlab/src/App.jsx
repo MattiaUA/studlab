8281e2f7ab34745ef6c4a84afb420a1d4d4818c7
Marcos150 <marcosruizrubio@gmail.com>
2024-04-19 11:49:25 +0200

    Cambiada localización del navigation bar del document-page a la app

--- studlab/src/App.jsx
+++ studlab/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import DocumentPage from './pages/document-page';
+import NavigationBar from "./components/navigation-bar";
 
 import './App.css';
 import UsersData from './exampledata/Users.json';
@@ -9,11 +10,14 @@ import DocumentData from './exampledata/Documents.json'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<DocumentPage data={DocumentData["documentos"][1]}/>} />
-      </Routes>
-    </Router>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<DocumentPage data={DocumentData["documentos"][1]} />} />
+        </Routes>
+      </Router>
+      <NavigationBar></NavigationBar>
+    </div>
   );
 }
 
--- studlab/src/pages/document-page.jsx
+++ studlab/src/pages/document-page.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 import DocumentVisualizer from "../components/document-visualizer";
-import NavigationBar from "../components/navigation-bar";
 
 function DocumentPage({data}){
     return(
-        <div>
-            <DocumentVisualizer data={data}></DocumentVisualizer>
-            <NavigationBar></NavigationBar>
-        </div>
+        <DocumentVisualizer data={data}></DocumentVisualizer>
     )
 }
 export default DocumentPage;
