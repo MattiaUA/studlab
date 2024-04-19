d8938c88b7bc8e0bd22f5de7229b49726dfe34e9
Marcos150 <marcosruizrubio@gmail.com>
2024-04-19 09:44:35 +0200

    Añadidos enlaces a la barra de navegación y creado el proyecto de android

--- studlab/src/components/navigation-bar.jsx
+++ studlab/src/components/navigation-bar.jsx
@@ -3,11 +3,21 @@ import React from 'react'
 function NavigationBar() {
   return (
     <div className="nav-bar">
+      <a href={`/`}>
         <img src="/home.png" alt="Home"></img>
+      </a>
+      <a href={`search`}>
         <img src="/search.png" alt="Search"></img>
+      </a>
+      <a href={`new`}>
         <img src="/plus.png" alt="New"></img>
+      </a>
+      <a href={`notifications/`}>
         <img src="/notification.png" alt="Notifications"></img>
+      </a>
+      <a href={`profile`}>
         <img src="/user.png" alt="Profile"></img>
+      </a>
     </div>
   )
 }
