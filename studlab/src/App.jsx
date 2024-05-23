67aee04d68eb223292b9e570aec6069eadccdb4d
Mattia Sorella <sorellamattia@gmail.com>
2024-05-23 11:41:22 +0200

    chore: Uploaded for camel's use

--- studlab/src/App.jsx
+++ studlab/src/App.jsx
@@ -11,6 +11,7 @@ import DocumentData from './exampledata/Documents.json'
 import Home from './components/layouts/home';
 import SearchPage from './components/layouts/searchPage';
 import NavigationBar from './components/partials/navigation-bar';
+import RelevantFiles from './components/partials/relevantFiles';
 
 function App() {
   if (Capacitor.isNativePlatform()) {
@@ -28,6 +29,7 @@ function App() {
           <Route path="/preview" element={<Home docData={DocumentData} userData={UsersData} />} />
           <Route path="/preview-carrousel" element={<Home docData={DocumentData} userData={UsersData} />} />
           <Route path="/search" element={<SearchPage docData={DocumentData} userData={UsersData} />} />
+          <Route path="/relevant-files" element={<RelevantFiles docData={DocumentData} userData={UsersData}/>} />
         </Routes>
       </Router>
     </div>
