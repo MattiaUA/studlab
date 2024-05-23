d48200a9706c070d71e8c87878327d8a9ad42f5d
Marcos150 <marcosruizrubio@gmail.com>
2024-05-23 15:53:30 +0200

    Implementado filtrado de documentos

--- studlab/src/components/layouts/home.jsx
+++ studlab/src/components/layouts/home.jsx
@@ -5,32 +5,35 @@ import UsuariosReel from '../partials/usuarios-reel'
 import NavigationBar from '../partials/navigation-bar';
 
 export default function Home(props) {
-    const { docData, userData } = props
+    const { docData, userData } = props;
+    const [filterDoc, setFilterDoc] = useState(docData);
     const [selectedNavItem, setSelectedNavItem] = useState("todos");
 
     const handleNavClick = (navItem) => {
         setSelectedNavItem(navItem);
+        if (navItem != "todos")
+            setFilterDoc({ 'documentos': docData["documentos"].filter(doc => doc.carrera.toLowerCase().includes(navItem)) });
+        else
+            setFilterDoc(docData);
     };
 
-    const prevData = parsePrev(docData, userData)
+    const prevData = parsePrev(filterDoc, userData)
     return (
         <>
             <ul className='categorias'>
                 <li><button onClick={() => handleNavClick("todos")} className={selectedNavItem === "todos" ? "active-home-nav" : ""}>Todos</button></li>
-                <li><button onClick={() => handleNavClick("sociales")} className={selectedNavItem === "sociales" ? "active-home-nav" : ""}>Ciencias sociales</button></li>
-                <li><button onClick={() => handleNavClick("artes")} className={selectedNavItem === "artes" ? "active-home-nav" : ""}>Artes y humanidades</button></li>
-                <li><button onClick={() => handleNavClick("ingenieria")} className={selectedNavItem === "ingenieria" ? "active-home-nav" : ""}>Ingenierías</button></li>
-                <li><button onClick={() => handleNavClick("educacion")} className={selectedNavItem === "educacion" ? "active-home-nav" : ""}>Educación</button></li>
+                <li><button onClick={() => handleNavClick("ciencia")} className={selectedNavItem === "ciencia" ? "active-home-nav" : ""}>Ciencia</button></li>
+                <li><button onClick={() => handleNavClick("economía")} className={selectedNavItem === "economía" ? "active-home-nav" : ""}>Economía</button></li>
+                <li><button onClick={() => handleNavClick("ingeniería")} className={selectedNavItem === "ingeniería" ? "active-home-nav" : ""}>Ingeniería</button></li>
+                <li><button onClick={() => handleNavClick("biología")} className={selectedNavItem === "biología" ? "active-home-nav" : ""}>Biología</button></li>
+                <li><button onClick={() => handleNavClick("química")} className={selectedNavItem === "química" ? "active-home-nav" : ""}>Química</button></li>
+                <li><button onClick={() => handleNavClick("psicología")} className={selectedNavItem === "psicología" ? "active-home-nav" : ""}>Psicología</button></li>
             </ul>
             <UsuariosReel users={userData} />
             <div className='previews'>
-                <Preview data={prevData[0]} />
-                <Preview data={prevData[1]} />
-                <Preview data={prevData[2]} />
-                <Preview data={prevData[3]} />
-                <Preview data={prevData[4]} />
-                <Preview data={prevData[5]} />
-                <Preview data={prevData[6]} />
+                {prevData.map((prev, index) => (
+                    <Preview key={index} data={prev} />
+                ))}
                 <NavigationBar />
             </div>
         </>
