0275ff50dd8ee26947ba2f9c4bb4e9173560eed8
Marcos150 <marcosruizrubio@gmail.com>
2024-05-24 11:50:19 +0200

    Creado proyecto de back

---
+++ studlabBackend/src/main/java/edu/ua/studlab/studlabbackend/StudlabBackendApplication.java
@@ -0,0 +1,15 @@
+package edu.ua.studlab.studlabbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudlabBackendApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(StudlabBackendApplication.class, args);
+    }
+
+}
---
+++ studlabBackend/src/test/java/edu/ua/studlab/studlabbackend/StudlabBackendApplicationTests.java
@@ -0,0 +1,15 @@
+package edu.ua.studlab.studlabbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudlabBackendApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
