81f00d47c0c023b473f86c743c4064e9e3365d10
= <=>
2024-05-24 10:49:42 +0200

    10.49

--- studlab/src/components/layouts/documentPage.jsx
+++ studlab/src/components/layouts/documentPage.jsx
@@ -1,21 +1,37 @@
 import React from "react";
+import { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
 import NavigationBar from "../partials/navigation-bar";
 import DocumentVisualizer from "../partials/document-visualizer";
+import { Preferences } from '@capacitor/preferences';
 
 function DocumentPage({docData}) {
 
+    const [docs, setDocs] = useState(docData);
     const { id } = useParams();
-    console.log(docData)
-    const document = docData.documentos.find(doc => doc.id.toString() === id.toString());
+    const [doc, setDoc] = useState("");
 
-    if (!document) {
+    useEffect(() => {
+        const loadDocumentData = async () => {
+          const { value } = await Preferences.get({ key: 'DocumentData' });
+          if (value) {
+            setDocs(JSON.parse(value));
+            console.log("DOCS",docs)
+            const tmpDoc = docs.documentos.find(doc => doc.id.toString() === id.toString());
+            console.log("DOC",tmpDoc)
+            setDoc(tmpDoc);
+          }
+        };
+        loadDocumentData();
+    },[docData]);
+
+    if (!doc) {
         return <div>No se encontró el documento.</div>;
     }
 
     return (
         <div className="document-page">
-            <DocumentVisualizer data={document} />
+            { doc && <DocumentVisualizer data={doc} />}
             <NavigationBar />
         </div>
     );
